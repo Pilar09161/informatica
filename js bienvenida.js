92a47efcbda5115130d22d8551b3274510075e38
Pilar09161 <p.delosrios@colegiocalasanz.edu.ar>
2025-09-26 15:17:54 -0300

    Add files via upload

---
+++ js bienvenida.js
@@ -0,0 +1,4 @@
+function pedirnombre (){
+    let nombre=prompt("ingrese su nombre");
+    alert("Bienvenida"  + nombre + "a la clase de JS");
+}
---
+++ sumadora.js
@@ -0,0 +1,12 @@
+function sumarnumero(){
+    let num1=prompt("Ingrese el primer número:");
+    let num2=prompt("Ingrese el Segundo número:");
+    num1= Number(num1);
+    num2= Number(num2);
+    if(!isNaN(num1)&&!isNaN(num2)){
+        let suma=num1+num2;
+        alert("Resultado:"+Suma);
+     } else{
+        alert("Ingrese solo numeros");
+        }
+    }
